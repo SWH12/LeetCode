d22e1bc3ff46d8e9660b61f764bcf674647f1673
SWH12 <2996259397@qq.com>
2020-10-06 15:19:54 +0800

    xiugai

--- test/src/main/java/t28/a1.java
+++ test/src/main/java/t28/a1.java
@@ -1,5 +1,9 @@
 package t28;
 
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
+import java.lang.reflect.Method;
+
 public class a1 {
     public int strStr(String haystack, String needle) {
         if (needle.length() == 0 ) return 0;
@@ -12,5 +16,22 @@ public class a1 {
         String str1 = "haaaa";
         String str2 = "ll";
         System.out.println(a.strStr(str1,str2));
+        Test123 test = new Test123();
+
+        Class testId = test.getClass();
+        System.out.println(testId.getName());
+        System.out.println(testId.getPackage());
+        Method[] methods =  testId.getMethods();
+        for (int i=0; i<methods.length; i++) {
+            System.out.println(methods[i].getName());
+        }
     }
+}
+
+class Test123 {
+    public void pf() {
+        System.out.println("print out test class!");
+    }
+
+    public void t1() {}
 }
